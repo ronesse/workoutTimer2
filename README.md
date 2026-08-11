a2359fbc65ce943523775be9d41488d6578542c1
ronesse <121096345+ronesse@users.noreply.github.com>
2026-08-11 13:59:44 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Inneholder e-postinnlogging og synkronisering av aktivitetsdata til Supabase. Lo
 Last opp alle filene direkte i roten av GitHub-repository `WorkoutTimer2`.
 
 Viktig: Tabellen `public.activities` og RLS-policyene må være opprettet i Supabase.
+
+
+## v2.1 FIXED
+- Rettet JavaScript-feil i db.js som gjorde at konto-knappen ikke reagerte.
+- db.js er nå inkludert i PWA-cache.
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="workouttimer2-supabase-v2";
+const CACHE_NAME="workouttimer2-supabase-v2-1-fixed";
 const ASSETS=["./","./index.html","./style.css","./db.js","./app.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
